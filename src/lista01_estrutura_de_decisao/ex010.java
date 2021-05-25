64da2f1eafa5b680ba31a9f94acc720d76e98fee
renan <renanrosetti@gmail.com>
2021-05-25 16:43:02 -0300

    lista01

--- src/lista01_estrutura_de_decisao/ex010.java
+++ src/lista01_estrutura_de_decisao/ex010.java
@@ -9,6 +9,6 @@ public class ex010 {
         float tempCels = myObj.nextFloat();
         float tempFah = (tempCels * 9 / 5) + 32;
         System.out.println("Temperatura em Fahrenheit: " + tempFah);
-        //W
+        //Wwww
     }
 }
